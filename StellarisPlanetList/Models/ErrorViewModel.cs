8e317c775964893c36fa31e801794d0ce0e800df
John Uthus <john.a.uthus.jr1@gmail.com>
2021-07-29 08:59:19 -0700

    Add project files.

---
+++ StellarisPlanetList/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StellarisPlanetList.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
