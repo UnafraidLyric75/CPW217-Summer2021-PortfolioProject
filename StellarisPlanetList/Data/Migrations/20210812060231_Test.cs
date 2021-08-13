4ae724cf4e8a6f5022d8c3d2183a74bf4f80a5c5
John Uthus <63675144+UnafraidLyric75@users.noreply.github.com>
2021-08-12 20:48:21 -0700

    Created models to have database modeled after, set up DB context and start up so i dont have to figure that one out later on Migration of database now exists Horay (#10)

---
+++ StellarisPlanetList/Data/Migrations/20210812060231_Test.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StellarisPlanetList.Data.Migrations
+{
+    public partial class Test : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Planets",
+                columns: table => new
+                {
+                    PlanetId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PlanetName = table.Column<string>(maxLength: 180, nullable: false),
+                    PlanetType = table.Column<string>(maxLength: 180, nullable: false),
+                    PlanetSize = table.Column<int>(maxLength: 3, nullable: false),
+                    PlanetPercenage = table.Column<int>(maxLength: 3, nullable: false),
+                    PlanetEffects = table.Column<string>(maxLength: 300, nullable: true),
+                    UserId = table.Column<int>(nullable: false),
+                    PlanetNotes = table.Column<string>(maxLength: 4000, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Planets", x => x.PlanetId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(nullable: true),
+                    Username = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true),
+                    DateOfBirth = table.Column<DateTime>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Planets");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ StellarisPlanetList/Models/PlanetViewModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace StellarisPlanetList.Models
+{
+    public class PlanetViewModel
+    {
+        [Key]
+        public int PlanetId { get; set; }
+
+        /// <summary>
+        /// Nmae of the planet in the game
+        /// Which is either a special name like Axel
+        /// Or a name like Sol 3 3rd planet from the star sol
+        /// </summary>
+        [Required]
+        [StringLength(180)]
+        public string PlanetName { get; set; }
+
+        /// <summary>
+        /// Thye wiki does not list all planets avliabale do not use it
+        /// This tells you what type of planet it is
+        /// It can be Gaia, Tomb, Tropical, Continental, Ocean, Arctic, Tundra,
+        /// Alpine, Desert, Arid, Savanna, Relic, archaeological, Terrafromable,
+        /// and Driod/Robot theres other planet types but you can settle pops on them
+        /// pops being population meaning thier essentialy useless
+        /// We also have ring worlds, habbittates and other ones which will be includined
+        /// meaning it will be open ended and no drop downs (mods also have special planets)
+        /// </summary>
+        [Required]
+        [StringLength(180)]
+        public string PlanetType { get; set; }
+
+        /// <summary>
+        /// this just refers to the planet size for pop to settle on
+        /// i belive its anywhere from 4 but ill set it as one since its hard to get a number
+        /// to 25 standard size but ive seen them get really high so it well have no max size
+        /// Ive seen people cliam 28, 30, and some other ones can get up to 120
+        /// again no drop down but min number will be set to be one or greater and it cant go over 360
+        /// </summary>
+        [Required]
+        [StringLength(3)]
+        public int PlanetSize { get; set; }
+
+        /// <summary>
+        /// What is the % your species will like the planet from 0% to 100% it can go over 
+        /// 100 but you dont get bonuses from that so it be a 1 to 100 type in box
+        /// </summary>
+        [StringLength(3)]
+        public int PlanetPercenage { get; set; }
+
+        /// <summary>
+        /// These are effect the planet can get such as high gravity, low gravitiy
+        /// mega fuanna, Aleins, a beep saying its getting dark and my battery is getting low,
+        /// and Holy world, as its a moddifer not a planet this will be separate from notes.
+        /// </summary>
+        [StringLength(300)]
+        public string PlanetEffects { get; set; }
+
+        /// <summary>
+        /// the number that links this planet to a user, as the user creates
+        /// a planet the planet becomes linked to the user via a number.
+        /// </summary>
+        [Required]
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// implermented now for issue number 4, note section for each planet but 
+        /// i will have to incorptrate it unqiuely into the planet view section
+        /// Most likley will open a pop up to display information or a scroll box of some sort undeicded as of now
+        /// </summary>
+        [StringLength(4000)]
+        public string PlanetNotes { get; set; }
+    }
+}
