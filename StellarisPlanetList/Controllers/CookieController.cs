bd7c3f6bd6fcbf00707fc88991b9dedf4ff313c7
John Uthus <63675144+UnafraidLyric75@users.noreply.github.com>
2021-08-24 11:34:51 -0700

    Webpage design (#13)
    * Create cshtml Login and Register pages
    
    * Updated Login and register pages, upadting database to apply migrations
    
    * updated database
    
    * added cookie model and updated startup
    
    * moved login to layout added Ihttp as well
    
    * updated launchsettings
    
    * Fixed Cookies
    
    * re created login partial
    
    * Login and logout works
    
    * Created everything for a index view of planets
    
    * final web desgin made

---
+++ StellarisPlanetList/Controllers/CookieController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StellarisPlanetList.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StellarisPlanetList.Controllers
+{
+    public class CookieController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IHttpContextAccessor _httpContext;
+
+        public CookieController(ApplicationDbContext context, IHttpContextAccessor httpContext)
+        {
+            _context = context;
+            _httpContext = httpContext;
+        }
+
+        public IActionResult Summary()
+        {
+            return View();
+        }
+    }
+}
---
+++ StellarisPlanetList/Data/PlanetDB.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using StellarisPlanetList.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace StellarisPlanetList.Data
+{
+    public class PlanetDB
+    {
+        public async static Task<int> GetTotalProductsAsync(ApplicationDbContext _context)
+        {
+            return await (from p in _context.Planets
+                          select p).CountAsync();
+        }
+
+        public async static Task<List<PlanetViewModel>> GetProductsAsync(ApplicationDbContext _context, int pageSize, int pageNum)
+        {
+            return await (from p in _context.Planets
+                          orderby p.PlanetName ascending
+                          select p)
+                       .Skip(pageSize * (pageNum - 1))
+                       .Take(pageSize)
+                       .ToListAsync();
+        }
+
+        public async static Task<PlanetViewModel> AddProductAsync(ApplicationDbContext _context, PlanetViewModel p)
+        {
+            _context.Planets.Add(p);
+            await _context.SaveChangesAsync();
+            return p;
+        }
+
+        public static async Task<PlanetViewModel> GetProductAsync(ApplicationDbContext context, int prodid)
+        {
+            PlanetViewModel p = await (from products in context.Planets
+                               where products.PlanetId == prodid
+                               select products).SingleAsync();
+
+            return p;
+        }
+    }
+}
